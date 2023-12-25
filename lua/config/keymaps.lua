c0869d7eccb4cdbc937f999cc76a0b586cbb23c2
Ben Cooper <mnib.mnib@gmail.com>
2023-12-25 23:47:49 +0200

    keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,30 @@
 -- Add any additional keymaps here
 
 vim.keymap.set({ "n", "x" }, "U", "<cmd>redo<cr>")
+vim.keymap.set({ "n", "x" }, "gw", "*N", { desc = "Search word under cursor" })
+-- Better viewing
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+vim.keymap.set("n", "g,", "g,zvzz")
+vim.keymap.set("n", "g;", "g;zvzz")
+
+-- Scrolling
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Paste
+vim.keymap.set("n", "]p", "o<Esc>p", { desc = "Paste below" })
+vim.keymap.set("n", "]P", "O<Esc>p", { desc = "Paste above" })
+
+-- Insert blank line
+vim.keymap.set("n", "]<Space>", "o<Esc>")
+vim.keymap.set("n", "[<Space>", "O<Esc>")
+
+-- Auto indent
+vim.keymap.set("n", "i", function()
+  if #vim.fn.getline(".") == 0 then
+    return [["_cc]]
+  else
+    return "i"
+  end
+end, { expr = true })
