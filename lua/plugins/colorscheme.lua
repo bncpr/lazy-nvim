2f11fab45756af7f463b3ae3687e9ec6ae7b546e
Ben Cooper <mnib.mnib@gmail.com>
2023-12-26 23:35:42 +0200

    add oxocarbon colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,13 +3,13 @@ return {
     "maxmx03/dracula.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      local dracula = require("dracula")
-
-      dracula.setup()
-
-      vim.cmd.colorscheme("dracula")
-    end,
+    -- config = function()
+    --   local dracula = require("dracula")
+    --
+    --   dracula.setup()
+    --
+    --   vim.cmd.colorscheme("dracula")
+    -- end,
   },
   {
     "LazyVim/LazyVim",
@@ -17,4 +17,5 @@ return {
       colorscheme = "dracula",
     },
   },
+  { "nyoom-engineering/oxocarbon.nvim" },
 }
