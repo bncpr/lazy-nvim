a6b6d2613d8a6d04a9246f6b4a1354477c084b69
Ben Cooper <mnib.mnib@gmail.com>
2023-12-26 23:38:32 +0200

    neotree close on open + pick window

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,16 +1,23 @@
 return {
   {
-    {
-      "nvim-neo-tree/neo-tree.nvim",
-      opts = {
-        event_handlers = {
-          {
-            event = "file_opened",
-            ---@diagnostic disable-next-line: unused-local
-            handler = function(file_path)
-              require("neo-tree.command").execute({ action = "close" })
-            end,
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        window = {
+          mappings = {
+            ["s"] = "vsplit_with_window_picker",
+            ["S"] = "split_with_window_picker",
           },
+        },
+      },
+      event_handlers = {
+        {
+          event = "file_opened",
+          ---@diagnostic disable-next-line: unused-local
+          handler = function(file_path)
+            require("neo-tree.command").execute({ action = "close" })
+          end,
         },
       },
     },
