056e34c60a96b55c22b15211e6aba39318c12ab2
Ben Cooper <mnib.mnib@gmail.com>
2023-12-26 13:06:37 +0200

    neotree auto close

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,21 @@
 return {
   {
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      opts = {
+        event_handlers = {
+          {
+            event = "file_opened",
+            ---@diagnostic disable-next-line: unused-local
+            handler = function(file_path)
+              require("neo-tree.command").execute({ action = "close" })
+            end,
+          },
+        },
+      },
+    },
+  },
+  {
     "ckolkey/ts-node-action",
     event = "VeryLazy",
     dependencies = { "nvim-treesitter" },
