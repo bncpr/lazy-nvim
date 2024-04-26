58da945305e35b0378b9cf3a6964d9c2e22a2081
Ben Cooper <benc3804@gmail.com>
2024-04-26 10:11:33 +0300

    change autocmp

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -59,12 +59,12 @@ return {
   },
   -- Use <tab> for completion and snippets (supertab)
   -- first: disable default <tab> and <s-tab> behavior in LuaSnip
-  {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
-  },
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   keys = function()
+  --     return {}
+  --   end,
+  -- },
   -- then: setup supertab in cmp
   {
     "hrsh7th/nvim-cmp",
@@ -76,28 +76,30 @@ return {
       local luasnip = require("luasnip")
       local cmp = require("cmp")
 
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- this way you will only jump inside the snippet region
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
+      opts.completion = vim.tbl_extend("force", opts.completion, { autocomplete = false })
+
+      -- opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      --   ["<Tab>"] = cmp.mapping(function(fallback)
+      --     if cmp.visible() then
+      --       cmp.select_next_item()
+      --       -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+      --       -- this way you will only jump inside the snippet region
+      --     elseif luasnip.expand_or_locally_jumpable() then
+      --       luasnip.expand_or_jump()
+      --     else
+      --       fallback()
+      --     end
+      --   end, { "i", "s" }),
+      --   ["<S-Tab>"] = cmp.mapping(function(fallback)
+      --     if cmp.visible() then
+      --       cmp.select_prev_item()
+      --     elseif luasnip.jumpable(-1) then
+      --       luasnip.jump(-1)
+      --     else
+      --       fallback()
+      --     end
+      --   end, { "i", "s" }),
+      -- })
     end,
   },
 }
